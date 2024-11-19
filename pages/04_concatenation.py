1a93de6a26c0f76a09f781cd6fb5387807947082
CARDONAJOSE <CARDONAJOSE>
2024-11-19 09:06:31 +0100

    concatenation functionel

--- pages/04_concatenation.py
+++ pages/04_concatenation.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import app as app
 
-st.subheader("Concatener les dataframe")
+#st.subheader("Concatener les dataframe")
 
 if 'data_numeric' not in st.session_state:
     st.session_state.data_numeric = app.data_numeric.copy()  # Récupérer les données numériques
@@ -14,22 +14,19 @@ if 'data_non_numeric' not in st.session_state:
     st.session_state.data_non_numeric = app.data_non_numeric.copy()  # Récupérer les données non numériques
 
 # Titre de l'application
-st.subheader("concateneation du dataframe y exportation en archivo csv 📊")
+st.subheader("Concatenation du dataframe et exportation en archivo csv 📊")
 
-st.subheader("dataframe numerique")
+st.subheader("Dataframe numerique")
 st.dataframe(st.session_state.data_numeric)
 
-st.subheader("dataframe non numerique")
+st.subheader("Dataframe non numerique")
 st.dataframe(st.session_state.data_non_numeric)
 
 #st.subheader("dataframe numerique")
 
 if st.button("Concatener les DataFrame"):
+    st.session_state.data_concat= pd.concat([st.session_state.data_non_numeric, st.session_state.data_numeric], axis=1)
+else:
+    st.warning("Aucune colonne sélectionnée pour la suppression.")
 
-# le dataframe sans le columns suprimer
-    if colonnes_disponibles:
-        st.session_state.data_numeric.drop(colonnes_select, axis='columns', inplace=True)
-        st.success(f"Les colonnes {list(colonnes_select)} ont été supprimées avec succès!")
-    else:
-        st.warning("Aucune colonne sélectionnée pour la suppression.")
-
+st.dataframe(st.session_state.data_concat)
